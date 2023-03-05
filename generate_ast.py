133ccf0a9a8d577858c63df6060c95e1e76ac5c5
chpi7 <11175879+chpi7@users.noreply.github.com>
2023-03-05 11:16:14 +0100

    rename

--- generate_ast.py
+++ generate_ast.py
@@ -8,7 +8,7 @@ IFDEF_NAME = "carl_ast_h"
 INCLUDES = ["<sstream>", "<fstream>", "<memory>", "<list>", '"carl/scanner.h"', '"carl/common.h"']
 NAMESPACE = "carl"
 FORWARD_DECLS = ["class AstNodeVisitor;"]
-REPLACEMENTS = {"@spr": "std::shared_ptr", "@list": "std::list"}
+REPLACEMENTS = {"@ptr": "std::shared_ptr", "@list": "std::list"}
 
 ASTNODE = """class AstNode {
    public:
@@ -23,19 +23,19 @@ TYPES = [
     "Expression() : AstNode",
 
     "Type(Token name) : AstNode",
-    "FormalParam(Token name, @spr<Type> type) : AstNode",
-    "FnDecl(Token name, @list<@spr<AstNode>> formals, @spr<AstNode> body) : AstNode",
+    "FormalParam(Token name, @ptr<Type> type) : AstNode",
+    "FnDecl(Token name, @list<@ptr<AstNode>> formals, @ptr<AstNode> body) : AstNode",
 
     # TODO: rename this to LetDecl
-    "LetStmt(Token name, @spr<AstNode> initializer) : Statement",
-    "ExprStmt(@spr<AstNode> expr) : Statement",
-    "ReturnStmt(@spr<AstNode> expr) : Statement",
-    "WhileStmt(@spr<AstNode> condition, @spr<AstNode> body) : Statement",
-    "Block(@list<@spr<AstNode>> declarations) : Statement",
+    "LetStmt(Token name, @ptr<AstNode> initializer) : Statement",
+    "ExprStmt(@ptr<AstNode> expr) : Statement",
+    "ReturnStmt(@ptr<AstNode> expr) : Statement",
+    "WhileStmt(@ptr<AstNode> condition, @ptr<AstNode> body) : Statement",
+    "Block(@list<@ptr<AstNode>> declarations) : Statement",
 
-    "Assignment(@spr<AstNode> target, @spr<Expression> expr) : Expression",
-    "Binary(Token op, @spr<Expression> lhs, @spr<Expression> rhs) : Expression",
-    "Unary(Token op, @spr<Expression> operand) : Expression",
+    "Assignment(@ptr<AstNode> target, @ptr<Expression> expr) : Expression",
+    "Binary(Token op, @ptr<Expression> lhs, @ptr<Expression> rhs) : Expression",
+    "Unary(Token op, @ptr<Expression> operand) : Expression",
     "Variable(Token name) : Expression",
     "Literal(Token value) : Expression",
     "String(Token value) : Expression",
